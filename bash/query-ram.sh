61ce32dbf1131cdf6387b8533586bd28a3dc49a8
Brucknem <mbruckner94@gmail.com>
2020-08-17 18:43:07 +0200

    Query RAM

---
+++ bash/query-ram.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+eol=$'\n'
+raw_values_raw="type ""$(free -wm)"
+filename="ram-query.csv"
+
+tail_length=1
+if [ -f "$filename" ]; then
+    tail_length=2
+fi
+
+raw_values_raw=$(echo "$raw_values_raw" | tail -n +$tail_length)
+while IFS= read -r line
+do
+    # echo "$line"
+    fixed_line=$(echo $line | tr -s '[:blank:]' ';')
+    fixed_line=$(echo "${fixed_line//:}")
+    # echo "$fixed_line"
+    raw_values="$fixed_line$eol"
+    echo $raw_values >> $filename
+done < <(printf '%s\n' "$raw_values_raw")
