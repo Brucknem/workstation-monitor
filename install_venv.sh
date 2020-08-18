b7d4b8878da871a93ab1db10ef9285b56bd4ff7e
Brucknem <mbruckner94@gmail.com>
2020-08-18 22:46:37 +0200

    Removed trusted host

--- install_venv.sh
+++ install_venv.sh
@@ -2,4 +2,4 @@
 
 virtualenv --python=python3.8 --no-site-packages venv
 source venv/bin/activate
-pip install --no-cache-dir --upgrade --trusted-host pypi.org -r requirements.txt 
+pip install --no-cache-dir --upgrade -r requirements.txt 
