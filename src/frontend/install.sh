bf3d5934893ce947bd4527217d3b7dd21a59f608
Brucknem <mbruckner94@gmail.com>
2020-08-19 17:55:01 +0200

    Better install

--- src/frontend/install.sh
+++ src/frontend/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-virtualenv --python=python3.8 --no-site-packages ${BASH_SOURCE%/*}/venv
+MY_PATH="`dirname \"$0\"`"
+virtualenv --python=python3.8 --no-site-packages $MY_PATH/venv
 source venv/bin/activate
-pip install --no-cache-dir --upgrade -r ${BASH_SOURCE%/*}/requirements.txt 
+pip install --no-cache-dir --upgrade -r $MY_PATH/requirements.txt 
