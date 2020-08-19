45715040ed98ea0ffea2e1ad91d285f94605fec4
Brucknem <mbruckner94@gmail.com>
2020-08-19 09:48:24 +0200

    Added echo of log dir on startup

--- src/backend/workstation-monitor-service.sh
+++ src/backend/workstation-monitor-service.sh
@@ -4,6 +4,8 @@ modprobe asus-wmi-sensors
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 log_dir="/var/log/workstation-monitor/"$timestamp
 [ -d $log_dir ] || mkdir $log_dir -p
+echo "Logging to $log_dir"
+
 while true; do
     /bin/bash ${BASH_SOURCE%/*}/query-all.sh $log_dir
     sleep 1
