08d02be0d3b048f7add5e736b6cfe8f2cf9281bd
Brucknem <mbruckner94@gmail.com>
2020-09-08 11:29:49 +0200

    Fixed install and gpu query

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install python3-dev libsystemd-dev -y
 MY_PATH="`dirname \"$0\"`"
-virtualenv --python=python3.8 --no-site-packages $MY_PATH/venv
+virtualenv --python=python3.8 $MY_PATH/venv
 source $MY_PATH/venv/bin/activate
 pip install --no-cache-dir --upgrade -r $MY_PATH/requirements.txt 
