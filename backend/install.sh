6b8f5e5e1a7f2ad84503d10fe49c139cc069364a
Brucknem <mbruckner94@gmail.com>
2020-09-10 10:32:25 +0200

    Conditional install of the virtual env

--- backend/install.sh
+++ backend/install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install python3-dev libsystemd-dev -y
 MY_PATH="$(dirname "$0")"
-virtualenv --python=python3.7 $MY_PATH/venv
+virtualenv --python=python3.8 "$MY_PATH/venv" || virtualenv --python=python3.7 "$MY_PATH/venv" || virtualenv --python=python3.6 "$MY_PATH/venv"
 source "$MY_PATH/venv/bin/activate"
 pip install --no-cache-dir --upgrade -r $MY_PATH/requirements.txt
